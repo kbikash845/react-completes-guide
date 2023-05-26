2cb1cc19a107aa292b8a75a0ae9cfa0159c8cbc6
Bikash <kbikash845@gmail.com>
2023-05-26 22:22:29 +0530

    Changes

---
+++ src/Components/NewExpanses/New_Expanse.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./New_Expanse.css";
+import ExpanseForm from './ExpanseForm';
+
+const New_Expanse=()=> {
+
+  return (
+    <div className='new__expanse'>
+    <ExpanseForm/>
+      
+    </div>
+  )
+}
+
+export default New_Expanse
