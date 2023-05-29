e707e13fb17a7f697e10c63e47ff4108ee51d462
Bikash <kbikash845@gmail.com>
2023-05-29 11:58:50 +0530

    Submitting the forms

--- src/Components/NewExpanses/ExpanseForm.js
+++ src/Components/NewExpanses/ExpanseForm.js
@@ -1,21 +1,74 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import "./ExpanseForm.css"
 
 const ExpanseForm=() =>{
+   const[EnterTitle,setenterTitle]= useState()
+   const[Enteramount,setenteramount]= useState()
+   const[Enterdate,setenterdate]= useState()
+
+//    const [userInput,SetuserInput]=useState({
+//     EnterTitle:"",
+//     Enteramount:"",
+//     Enterdate:""
+//    })
+
+
+    const titleHandler=(event)=>{
+        setenterTitle(event.target.value)
+        // SetuserInput({
+        //     ...userInput,
+        //     EnterTitle:event.target.value,
+        // });
+
+    //    SetuserInput((prevState)=>{
+    //         return{...prevState,EnterTitle:event.target.value
+
+    //         }
+    //    })
+        
+    }
+    const amountHandler=(event)=>{
+        setenteramount(event.target.value)
+        // SetuserInput({
+        //     ...userInput,
+        //     Enteramount:event.target.value
+        // });
+    }
+    const dateHandler=(event)=>{
+        setenterdate(event.target.value)
+        // SetuserInput({
+        //     ...userInput,
+        //     Enterdate:event.target.value,
+        // });
+      }
+
+        const submitHandler=(e)=>{
+             e.preventDefault();
+             const expansedate={
+                title:EnterTitle,
+                amount:Enteramount,
+                date:new Date(Enterdate)
+             }
+             console.log(expansedate)
+             setenterTitle("")
+             setenteramount("")
+             setenterdate("")
+        }
+    
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
          <div className='new-expense__controls'>
-             <label>Ttitle</label>
-              <input type='text'/>
+             <label>Title</label>
+              <input type='text' value={EnterTitle} onChange={titleHandler}/>
             </div>
             <div className='new-expense__controls'>
              <label>Amount</label>
-              <input type='number' min="0.01" step="0.01"/>
+              <input type='number' min="0.01" step="0.01" value={Enteramount} onChange={amountHandler}/>
             </div>
             <div className='new-expense__controls'>
              <label>Date</label>
-              <input type='Date' min="2019-01-01" max="2024-01-01"/>
+              <input type='Date' min="2019-01-01" max="2024-01-01" value={Enterdate} onChange={dateHandler}/>
             </div>
       </div>
       <div className='new-expanse__action'>
