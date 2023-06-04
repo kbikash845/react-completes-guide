b56c9170d973c457ca1337f11baf116db05de504
Bikash <kbikash845@gmail.com>
2023-06-04 07:32:11 +0530

    Dummy_Expanse added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import ExpenseItem from './Components/Expanses/ExpenseItem';
@@ -5,45 +6,49 @@ import NewExpanses from './Components/NewExpanses/New_Expanse';
 import Expanse from './Components/Expenses/Expanse';
 
 
-
+const Dummy_Expanse=[
+  {
+    id:"e1",
+    title:"Toilet Paper",
+    amount:"94.12",
+    date:new Date(2020,7,14),
+    location:"Store"
+  },
+  {
+    id: "e2", 
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      location: "Shooping Mall",
+  },
+  {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: "Office",
+  },
+  {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: "Carpenter",
+  }]
 
 
 
 
 const App=() =>{
-  const expanses=[
-    {
-      id:"e1",
-      title:"Toilet Paper",
-      amount:"94.12",
-      date:new Date(2020,7,14),
-      location:"Store"
-    },
-    {
-      id: "e2", 
-        title: "New TV",
-        amount: 799.49,
-        date: new Date(2021, 2, 12),
-        location: "Shooping Mall",
-    },
-    {
-        id: "e3",
-        title: "Car Insurance",
-        amount: 294.67,
-        date: new Date(2021, 2, 28),
-        location: "Office",
-    },
-    {
-        id: "e4",
-        title: "New Desk (Wooden)",
-        amount: 450,
-        date: new Date(2021, 5, 12),
-        location: "Carpenter",
-    }]
+   const [expanses,setExpanse] =useState(Dummy_Expanse)
+ 
 
     const AddExpanseDatahandler=expanse=>{
-      console.log("App js");
-      console.log(expanse)
+      // console.log("App js");
+      // console.log(expanse)
+      setExpanse((prevExpenses)=>{
+        return [expanse, ...prevExpenses]
+      })
     }
 
   
